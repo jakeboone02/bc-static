17bd8b9876db629be99146163ad3616a218346f5
Jake Boone <jakeboone02@gmail.com>
2015-03-10 16:42:27 -0400

    Added gulp build

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var del = require("del");
+var gulp = require("gulp");
+var uglify = require("gulp-uglify");
+var rename = require("gulp-rename");
+var transform = require("vinyl-transform");
+var sourcemaps = require("gulp-sourcemaps");
+var browserify = require("browserify");
+var packageJSON = require("./package.json");
+
+// var getBundleName = function () {
+//   var version = packageJSON.version;
+//   var name = packageJSON.name;
+//   return version + "." + name + "." + "min";
+// };
+
+gulp.task("clean:js", function (cb) {
+  del(["js/bundle.*"], cb);
+});
+
+gulp.task("javascript", ["clean:js"], function () {
+  var browserified = transform(function(filename) {
+    var b = browserify(filename);
+    return b.bundle();
+  });
+
+  return gulp.src("./js/main.js")
+    .pipe(browserified)
+    .pipe(rename({ basename: "bundle" }))
+    .pipe(gulp.dest("./js/"))
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(uglify())
+    .pipe(rename({ extname: ".min.js" }))
+    .pipe(sourcemaps.write("./"))
+    .pipe(gulp.dest("./js/"));
+});
+
+gulp.task("default", ["javascript"]);
