4928d947a706e8dc11c6026f4f6d66531700d8aa
Jake Boone <jakeboone02@gmail.com>
2015-04-06 19:30:17 -0400

    Fixed build and upgraded react-bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@
 var del = require("del");
 var gulp = require("gulp");
 var data = require("./js/constants/data.js");
+var source = require("vinyl-source-stream");
+var buffer = require("vinyl-buffer");
 var uglify = require("gulp-uglify");
 var rename = require("gulp-rename");
 var data2obj = require("./js/constants/data2obj");
-var transform = require("vinyl-transform");
+var babelify = require("babelify");
 var sourcemaps = require("gulp-sourcemaps");
 var browserify = require("browserify");
 var packageJSON = require("./package.json");
@@ -27,14 +29,15 @@ gulp.task("data2obj", ["clean:js"], function(cb) {
 });
 
 gulp.task("js", ["clean:js", "data2obj"], function () {
-  var browserified = transform(function(filename) {
-    var b = browserify(filename);
-    return b.bundle();
+  var b = browserify({
+    entries: "./js/main.js",
+    debug: true,
+    transform: [babelify]
   });
 
-  return gulp.src("./js/main.js")
-    .pipe(browserified)
-    .pipe(rename({ basename: "bundle" }))
+  return b.bundle()
+    .pipe(source("bundle.js"))
+    .pipe(buffer())
     .pipe(gulp.dest("./js/"))
     .pipe(sourcemaps.init({loadMaps: true}))
       .pipe(uglify())
